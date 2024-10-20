1f0cbd5f5662bbb5aeced7fe4d255fe3dc548329
Saltman0 <baudoin.mathieu@gmail.com>
2024-10-20 13:37:04 +0200

    Drizzle config file added

---
+++ src/config/drizzle.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+    out: './migrations',
+    schema: './src/db/schema.ts',
+    dialect: 'postgresql',
+    verbose: process.env["IS_VERBOSE_ENABLED"] === "true",
+    dbCredentials: {
+        host: process.env["POSTGRES_HOST"] as string,
+        port: 5432,
+        user: process.env["POSTGRES_USER"] as string,
+        password: process.env["POSTGRES_PASSWORD"] as string,
+        database: process.env["POSTGRES_DB"] as string,
+        ssl: process.env["IS_SSL_ENABLED"] === "true"
+    }
+});
