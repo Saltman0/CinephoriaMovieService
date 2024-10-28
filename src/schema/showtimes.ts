feceba9675a7b959c5a557953b149b7c349b111c
Saltman0 <baudoin.mathieu@gmail.com>
2024-10-28 15:54:49 +0100

    Showtimes schema updated : movieId added

--- src/schema/showtimes.ts
+++ src/schema/showtimes.ts
@@ -1,8 +1,10 @@
 import * as drizzle from "drizzle-orm/pg-core";
+import { moviesTable } from "./movies";
 
 export const showtimesTable = drizzle.pgTable("showtimes", {
     id: drizzle.integer().primaryKey().generatedAlwaysAsIdentity(),
     startTime: drizzle.date().notNull(),
     endTime: drizzle.date().notNull(),
-    price: drizzle.integer().notNull()
+    price: drizzle.integer().notNull(),
+    movieId: drizzle.integer().references(() => moviesTable.id)
 });
