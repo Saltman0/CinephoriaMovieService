2850e83560edf38ac7addfb513f3bbe59f8aa32f
Saltman0 <baudoin.mathieu@gmail.com>
2024-10-29 11:42:00 +0100

    server.ts file updated

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,8 @@
-import express from "express";
+import {app, logger, port} from "./app";
 
-const app = express();
-const port = 3000;
-
-// Middleware to parse JSON data
-app.use(express.json());
-
-// Placeholder data
-let movies = [
-  { id: 1, title: "Inception", director: "Christopher Nolan", year: 2010 },
-  { id: 2, title: "Interstellar", director: "Christopher Nolan", year: 2014 },
-  { id: 3, title: "The Matrix", director: "The Wachowskis", year: 1999 },
-];
-
-app.get("/movies", (req: any, res: any) => {
-  res.json(movies);
-});
-
-app.get("/movies/:id", (req: any, res: any) => {
-  res.json(movies);
-});
+logger.info("test");
+logger.warn("alerte");
+logger.error("erreur 4565");
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
