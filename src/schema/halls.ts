e9b3bdc552610fefb88ec017b3398da3ff6582fa
Saltman0 <baudoin.mathieu@gmail.com>
2024-10-31 11:52:42 +0100

    showtimes and halls schemas fixed relation

--- src/schema/halls.ts
+++ src/schema/halls.ts
@@ -1,11 +1,9 @@
 import * as drizzle from "drizzle-orm/pg-core";
-import { showtimesTable } from "./showtimes";
 import { cinemasTable } from "./cinemas";
 
 export const hallsTable = drizzle.pgTable("halls", {
     id: drizzle.integer().primaryKey().generatedAlwaysAsIdentity(),
     number: drizzle.integer().notNull(),
     projectionQuality: drizzle.varchar(),
-    showtimeId: drizzle.integer().references(() => showtimesTable.id, {onDelete: "cascade"}).notNull(),
     cinemaId: drizzle.integer().references(() => cinemasTable.id, {onDelete: "cascade"}).notNull()
 });
--- src/schema/showtimes.ts
+++ src/schema/showtimes.ts
@@ -1,10 +1,12 @@
 import * as drizzle from "drizzle-orm/pg-core";
 import { moviesTable } from "./movies";
+import { hallsTable } from "./halls";
 
 export const showtimesTable = drizzle.pgTable("showtimes", {
     id: drizzle.integer().primaryKey().generatedAlwaysAsIdentity(),
     startTime: drizzle.timestamp().notNull(),
     endTime: drizzle.timestamp().notNull(),
     price: drizzle.integer().notNull(),
-    movieId: drizzle.integer().references(() => moviesTable.id, {onDelete: "cascade"}).notNull()
+    movieId: drizzle.integer().references(() => moviesTable.id, {onDelete: "cascade"}).notNull(),
+    hallId: drizzle.integer().references(() => hallsTable.id, {onDelete: "cascade"}).notNull()
 });
