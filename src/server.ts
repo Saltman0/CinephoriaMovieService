298d79efb9cf58c55776d0e6062679001800f556
Saltman0 <baudoin.mathieu@gmail.com>
2024-11-04 16:36:41 +0100

    server.ts : console.log node_env added

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,12 @@ logger.info("test");
 logger.warn("alerte");
 logger.error("erreur 4565");
 
+if (process.env.NODE_ENV === 'development') {
+  logger.info('Running in development mode');
+} else {
+  logger.info('Not in development mode');
+}
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
