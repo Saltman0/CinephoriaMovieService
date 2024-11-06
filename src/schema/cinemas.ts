d8e17e809ccdf88b32152099aaeab2c017437918
Saltman0 <baudoin.mathieu@gmail.com>
2024-11-06 12:48:06 +0100

    Cinemas schema fixed

--- src/schema/cinemas.ts
+++ src/schema/cinemas.ts
@@ -3,7 +3,7 @@ import * as drizzle from "drizzle-orm/pg-core";
 export const cinemasTable = drizzle.pgTable("cinemas", {
     id: drizzle.integer().primaryKey().generatedAlwaysAsIdentity(),
     name: drizzle.varchar().notNull(),
-    adress: drizzle.varchar().notNull(),
+    address: drizzle.varchar().notNull(),
     postalCode: drizzle.integer().notNull(),
     city: drizzle.varchar().notNull(),
     phoneNumber: drizzle.varchar().notNull(),
