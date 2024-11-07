754aba3f08292a159ddd95594d8a58bec57c3a0f
Saltman0 <baudoin.mathieu@gmail.com>
2024-11-07 16:20:26 +0100

    Init migration updated

--- migrations/0000_init.sql
+++ migrations/0000_init.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS "categories" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "categories_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+CREATE TABLE IF NOT EXISTS "category" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "category_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"name" varchar NOT NULL,
-	CONSTRAINT "categories_name_unique" UNIQUE("name")
+	CONSTRAINT "category_name_unique" UNIQUE("name")
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "cinemas" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "cinemas_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+CREATE TABLE IF NOT EXISTS "cinema" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "cinema_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"name" varchar NOT NULL,
 	"address" varchar NOT NULL,
 	"postalCode" integer NOT NULL,
@@ -15,33 +15,33 @@ CREATE TABLE IF NOT EXISTS "cinemas" (
 	"closeHour" time NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "halls" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "halls_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+CREATE TABLE IF NOT EXISTS "hall" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "hall_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"number" integer NOT NULL,
 	"projectionQuality" varchar,
 	"cinemaId" integer NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "movies" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "movies_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+CREATE TABLE IF NOT EXISTS "movie" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "movie_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"title" varchar NOT NULL,
 	"description" text NOT NULL,
 	"minimumAge" integer,
 	"favorite" boolean,
 	"imageURL" varchar NOT NULL,
 	"categoryId" integer NOT NULL,
-	CONSTRAINT "movies_imageURL_unique" UNIQUE("imageURL")
+	CONSTRAINT "movie_imageURL_unique" UNIQUE("imageURL")
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "ratings" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "ratings_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+CREATE TABLE IF NOT EXISTS "rating" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "rating_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"number" integer NOT NULL,
 	"description" text NOT NULL,
 	"movieId" integer NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "showtimes" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "showtimes_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+CREATE TABLE IF NOT EXISTS "showtime" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "showtime_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
 	"startTime" timestamp NOT NULL,
 	"endTime" timestamp NOT NULL,
 	"price" integer NOT NULL,
@@ -50,31 +50,31 @@ CREATE TABLE IF NOT EXISTS "showtimes" (
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "halls" ADD CONSTRAINT "halls_cinemaId_cinemas_id_fk" FOREIGN KEY ("cinemaId") REFERENCES "public"."cinemas"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "hall" ADD CONSTRAINT "hall_cinemaId_cinema_id_fk" FOREIGN KEY ("cinemaId") REFERENCES "public"."cinema"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "movies" ADD CONSTRAINT "movies_categoryId_categories_id_fk" FOREIGN KEY ("categoryId") REFERENCES "public"."categories"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "movie" ADD CONSTRAINT "movie_categoryId_category_id_fk" FOREIGN KEY ("categoryId") REFERENCES "public"."category"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "ratings" ADD CONSTRAINT "ratings_movieId_movies_id_fk" FOREIGN KEY ("movieId") REFERENCES "public"."movies"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "rating" ADD CONSTRAINT "rating_movieId_movie_id_fk" FOREIGN KEY ("movieId") REFERENCES "public"."movie"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "showtimes" ADD CONSTRAINT "showtimes_movieId_movies_id_fk" FOREIGN KEY ("movieId") REFERENCES "public"."movies"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "showtime" ADD CONSTRAINT "showtime_movieId_movie_id_fk" FOREIGN KEY ("movieId") REFERENCES "public"."movie"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "showtimes" ADD CONSTRAINT "showtimes_hallId_halls_id_fk" FOREIGN KEY ("hallId") REFERENCES "public"."halls"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "showtime" ADD CONSTRAINT "showtime_hallId_hall_id_fk" FOREIGN KEY ("hallId") REFERENCES "public"."hall"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
