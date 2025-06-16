a5adb29b57e3232c3ced5327464289f6e6e2ba67
Saltman0 <baudoin.mathieu@protonmail.com>
2025-06-16 13:49:35 +0200

    Update movie.routes.ts

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -5,8 +5,8 @@ import passport from "../middleware/passport";
 const router: Router = Router();
 
 router.get("/movie", passport.authenticate("jwt", { session: false }), movieController.getMovies);
-router.get("/movie/last-movies", passport.authenticate("jwt", { session: false }), movieController.getLastMovies);
-router.get("/movie/favorite-movies", passport.authenticate("jwt", { session: false }), movieController.getFavoriteMovies);
+router.get("/movie/last-movies", movieController.getLastMovies);
+router.get("/movie/favorite-movies", movieController.getFavoriteMovies);
 router.get("/movie/:movieId", passport.authenticate("jwt", { session: false }), movieController.getMovieById);
 router.post("/movie", passport.authenticate("jwt", { session: false }), movieController.createMovie);
 router.put("/movie/:movieId", passport.authenticate("jwt", { session: false }), movieController.updateMovie);
