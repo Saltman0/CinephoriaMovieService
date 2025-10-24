f74e586184ae064f28d8c00116da6b9513b05c30
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-24 13:44:22 +0200

    ✨ Rating repository added

---
+++ src/repository/rating.repository.ts
@@ -0,0 +1,86 @@
+import {database} from "../config/database";
+import {eq} from "drizzle-orm/sql/expressions/conditions";
+import {asc} from "drizzle-orm/sql/expressions/select";
+import {rating} from "../schema/rating";
+import * as ratingFactory from "../factory/rating.factory";
+
+export async function findRatings() {
+    try {
+        return await database
+            .select()
+            .from(rating)
+            .orderBy(asc(rating.id));
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function findRatingById(id: number) {
+    try {
+        const result = await database
+            .select()
+            .from(rating)
+            .where(eq(rating.id, id));
+
+        if (result.length === 0) {
+            return null;
+        }
+
+        return result[0];
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function insertRating(
+    number: number,
+    description: string,
+    validated: boolean,
+    movieId: number,
+    userId: number
+) {
+    try {
+        const preparedInsertRating = await database
+            .insert(rating)
+            .values(ratingFactory.createRating(number, description, validated, movieId, userId))
+            .returning();
+
+        return preparedInsertRating[0];
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function updateRating(
+    id: number,
+    number: number,
+    description: string,
+    validated: boolean,
+    movieId: number,
+    userId: number
+) {
+    try {
+        const preparedUpdateRating = await database
+            .update(rating)
+            .set(ratingFactory.createRating(number, description, validated, movieId, userId))
+            .where(eq(rating.id, id))
+            .returning();
+
+        return preparedUpdateRating[0];
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function deleteRating(id: number) {
+    try {
+        const preparedDeleteRating = await database
+            .delete(rating)
+            .where(eq(rating.id, id))
+            .returning({id: rating.id});
+
+        return preparedDeleteRating[0];
+    } catch (error) {
+        throw error;
+    }
+}
